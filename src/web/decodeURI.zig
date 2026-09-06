982bb0bf7edf7deb6bfd0d11e76c1014023b54c2
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-06 21:46:47 +1000

    test(decodeURI): add test for S15.1.3.1_A1.12_T1.js
    Original test from https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.12_T1.js

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -88,7 +88,11 @@ const testing = std.testing;
 // test/built-ins/decodeURI/S15.1.3.1_A1.10_T1.js
 // Copyright 2009 the Sputnik authors. All rights reserved.
 // Licensed under the Test262 BSD license; see LICENSES/Test262.txt.
-test "decodeURIAlloc: invalid hex digits after a two-byte UTF-8 prefix" {
+//
+// info: |
+//  If B = 110xxxxx (n = 2) and string.charAt(k + 4) and
+//  string.charAt(k + 5) do not represent hexadecimal digits, throw URIError
+test "decodeURIAlloc: (n = 2) invalid hex digits after a two-byte UTF-8 prefix" {
     const intervals = [_][2]u21{
         .{ 0x00, 0x2F },
         .{ 0x3A, 0x40 },
@@ -123,9 +127,13 @@ test "decodeURIAlloc: invalid hex digits after a two-byte UTF-8 prefix" {
     }
 }
 
-// test/built-ins/decodeURI/S15.1.3.1_A1.11_T1.js
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.11_T1.js
-test "decodeURIAlloc: invalid hex digits on the second byte" {
+//
+// info: |
+//  If B = 1110xxxx (n = 3) and (string.charAt(k + 4) and
+//  string.charAt(k + 5)) or (string.charAt(k + 7) and
+//  string.charAt(k + 8)) do not represent hexadecimal digits, throw URIError
+test "decodeURIAlloc: (n = 3) invalid hex digits on the second byte" {
     const intervals = [_][2]u21{
         .{ 0x00, 0x2F },
         .{ 0x3A, 0x40 },
@@ -150,6 +158,90 @@ test "decodeURIAlloc: invalid hex digits on the second byte" {
             try input_writer.writeAll(encoded_code_point[0..encoded_len]);
             try input_writer.writeAll(encoded_code_point[0..encoded_len]);
             try input_writer.writeAll("%A0");
+
+            if (decodeURIAlloc(testing.allocator, input_writer.buffered())) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
+
+// https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.11_T2.js
+// info: |
+//  If B = 1110xxxx (n = 3) and (string.charAt(k + 4) and
+//  string.charAt(k + 5)) or (string.charAt(k + 7) and
+//  string.charAt(k + 8)) do not represent hexadecimal digits, throw URIError
+test "decodeURIAlloc: (n = 3) invalid hex digits on the third byte" {
+    const intervals = [_][2]u21{
+        .{ 0x00, 0x2F },
+        .{ 0x3A, 0x40 },
+        .{ 0x47, 0x60 },
+        .{ 0x67, 0xFFFF },
+    };
+
+    for (intervals) |interval| {
+        var code_point = interval[0];
+        while (code_point <= interval[1]) : (code_point += 1) {
+            // Test262 iterates UTF-16 code units, including lone surrogates.
+            // They have no valid UTF-8 representation, so they are outside
+            // the input domain of this byte-oriented API.
+            if (std.unicode.isSurrogateCodepoint(code_point)) continue;
+
+            var encoded_code_point: [4]u8 = undefined;
+            const encoded_len: usize = try std.unicode.utf8Encode(code_point, &encoded_code_point);
+
+            var input_buffer: [16]u8 = undefined;
+            var input_writer: std.Io.Writer = .fixed(&input_buffer);
+            try input_writer.writeAll("%E0%");
+            try input_writer.writeAll("%A0");
+            try input_writer.writeAll(encoded_code_point[0..encoded_len]);
+            try input_writer.writeAll(encoded_code_point[0..encoded_len]);
+
+            if (decodeURIAlloc(testing.allocator, input_writer.buffered())) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
+
+// https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.12_T1.js
+//
+// info: |
+//  If B = 11110xxx (n = 4) and (string.charAt(k + 4) and
+//  string.charAt(k + 5)) or (string.charAt(k + 7) and
+//  string.charAt(k + 8)) or (string.charAt(k + 10) and
+//  string.charAt(k + 11)) do not represent hexadecimal digits, throw URIError
+test "decodeURIAlloc: (n = 4) invalid hex digits on the 2nd byte" {
+    const intervals = [_][2]u21{
+        .{ 0x00, 0x2F },
+        .{ 0x3A, 0x40 },
+        .{ 0x47, 0x60 },
+        .{ 0x67, 0xFFFF },
+    };
+
+    for (intervals) |interval| {
+        var code_point = interval[0];
+        while (code_point <= interval[1]) : (code_point += 1) {
+            // Test262 iterates UTF-16 code units, including lone surrogates.
+            // They have no valid UTF-8 representation, so they are outside
+            // the input domain of this byte-oriented API.
+            if (std.unicode.isSurrogateCodepoint(code_point)) continue;
+
+            var encoded_code_point: [4]u8 = undefined;
+            const encoded_len: usize = try std.unicode.utf8Encode(code_point, &encoded_code_point);
+
+            var input_buffer: [20]u8 = undefined;
+            var input_writer: std.Io.Writer = .fixed(&input_buffer);
+            try input_writer.writeAll("%F0%");
+            try input_writer.writeAll(encoded_code_point[0..encoded_len]);
+            try input_writer.writeAll(encoded_code_point[0..encoded_len]);
+            try input_writer.writeAll("%A0%A0");
 
             if (decodeURIAlloc(testing.allocator, input_writer.buffered())) |decoded| {
                 testing.allocator.free(decoded);
