19d48594b51de352a5547381631cc10eec649b1e
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-06 22:05:14 +1000

    test(decodeURI): add test for S15.1.3.1_A1.2_T2.js
    Original test from https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.2_T2.js

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -442,3 +442,43 @@ test "decodeURIAlloc: invalid hexadecimal digit on the 2nd character after %" {
         }
     }
 }
+
+// https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.2_T2.js
+//
+// info: |
+//  If B = string.charAt(k+1) + string.charAt(k+2) do not represent
+//  hexadecimal digits, throw URIError
+test "decodeURIAlloc: invalid hexadecimal digit on the 1st character after %" {
+    const intervals = [_][2]u21{
+        .{ 0x00, 0x2F },
+        .{ 0x3A, 0x40 },
+        .{ 0x47, 0x60 },
+        .{ 0x67, 0xFFFF },
+    };
+
+    for (intervals) |interval| {
+        var code_point = interval[0];
+        while (code_point <= interval[1]) : (code_point += 1) {
+            // Test262 iterates UTF-16 code units, including lone surrogates.
+            // They have no valid UTF-8 representation, so they are outside
+            // the input domain of this byte-oriented API.
+            if (std.unicode.isSurrogateCodepoint(code_point)) continue;
+
+            var encoded_code_point: [4]u8 = undefined;
+            const encoded_len: usize = try std.unicode.utf8Encode(code_point, &encoded_code_point);
+
+            var input_buffer: [8]u8 = undefined;
+            var input_writer: std.Io.Writer = .fixed(&input_buffer);
+            try input_writer.writeAll("%");
+            try input_writer.writeAll("1");
+            try input_writer.writeAll(encoded_code_point[0..encoded_len]);
+
+            if (decodeURIAlloc(testing.allocator, input_writer.buffered())) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
