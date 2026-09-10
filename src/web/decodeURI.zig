0df9c4cc5fe4a664cb0bef859b55b6fd1a909a7b
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-10 22:53:07 +1000

    test(decodeURI): add test for S15.1.3.1_A1.13_T1.js
    Original test from: https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.13_T1.js

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -175,6 +175,7 @@ test "decodeURIAlloc: invalid hexadecimal digits in the second octet of a three-
 }
 
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.11_T2.js
+//
 // A `firstOctet` with the bit pattern `1110xxxx` makes `Decode` collect two
 // more percent-encoded octets. Both digits in the third octet's `%HH` escape
 // must be ASCII hexadecimal; otherwise `decodeURIAlloc` must return
@@ -350,7 +351,27 @@ test "decodeURIAlloc: invalid hexadecimal digits in the fourth octet of a four-b
 // sequence. The following `continuationByte` must have the pattern `10xxxxxx`;
 // otherwise the octets are not valid UTF-8 and `decodeURIAlloc` must return
 // `error.URIError`.
-test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence" {}
+test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence" {
+    for (0xC0..0xE0) |i| {
+        const indexA: u8 = @intCast(i);
+        for (0x00..0x80) |j| {
+            const indexB: u8 = @intCast(j);
+            var buffer: [6]u8 = undefined;
+            const input = try std.fmt.bufPrint(
+                &buffer,
+                "%{X:0>2}%{X:0>2}",
+                .{ indexA, indexB },
+            );
+
+            if (decodeURIAlloc(testing.allocator, input)) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
 
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.1_T1.js
 //
