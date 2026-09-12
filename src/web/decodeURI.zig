ad9896fbd5d218c17b6bedb8bcc4a804f03fc064
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-12 13:39:16 +1000

    test(decodeURI): complete S15.1.3.1_A1.14_T1.js test

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -379,13 +379,13 @@ test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence (se
 // sequence. The following `continuationByte` must have the pattern `10xxxxxx`;
 // otherwise the octets are not valid UTF-8 and `decodeURIAlloc` must return
 // `error.URIError`.
-test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence (secondOctect = 0xC0..0x100)" {
+test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence (secondOctet = 0xC0..0x100)" {
     for (0xC0..0xE0) |i| {
         const firstOctet: u8 = @intCast(i);
         for (0xC0..0x100) |j| {
-            const secondOctect: u8 = @intCast(j);
+            const secondOctet: u8 = @intCast(j);
             var buffer: [6]u8 = undefined;
-            const input = try std.fmt.bufPrint(&buffer, "%{X:0>2}%{X:0>2}", .{ firstOctet, secondOctect });
+            const input = try std.fmt.bufPrint(&buffer, "%{X:0>2}%{X:0>2}", .{ firstOctet, secondOctet });
 
             if (decodeURIAlloc(testing.allocator, input)) |decoded| {
                 testing.allocator.free(decoded);
@@ -397,8 +397,34 @@ test "decodeURIAlloc: invalid continuation byte in a two-byte UTF-8 sequence (se
     }
 }
 
-// TODO:
+
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.14_T1.js
+//
+// A `firstOctect` with the bit pattern `1110xxxx` indicates a three-byte UTF-8
+// sequence. The following `continuationByte` must have the pattern `10xxxxxx`;
+// otherwise the octects are not valid UTF-8 and `decodeURIAlloc` must return
+// `error.URIError`.
+test "decodeURIAlloc: invalid continuation byte in a three-byte UTF-8 sequence (secondOctet = 0x00..0x80)" {
+    for (0xE0..0xF0) |i| {
+        const firstOctet: u8 = @intCast(i);
+        for (0x00..0x80) |j| {
+            const secondOctet: u8 = @intCast(j);
+            var buffer: [6]u8 = undefined;
+            const input = try std.fmt.bufPrint(
+                &buffer,
+                "%{X:0>2}%{X:0>2}",
+                .{ firstOctet, secondOctet },
+            );
+
+            if (decodeURIAlloc(testing.allocator, input)) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try std.testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
 
 // TODO:
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.14_T2.js
