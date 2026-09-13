de4947268d28c9b235107cded9c4d2146c2e8a2e
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-13 17:05:18 +1000

    test(decodeURI): complete S15.1.3.1_A1.8_T1.js test

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -933,3 +933,21 @@ test "decodeURIAlloc: continuation byte must be prefixed with '%' (n = 2)" {
         }
     }
 }
+
+// https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.8_T1.js
+//
+// An escape sequence (`1110xxxx1) must have the next continuation bytes be prefixed with '%'
+test "decodeURIAlloc: continuation byte must be prefixed with '%' (n = 3) - invalid 2nd octet" {
+    for (0xE0..0xF0) |i| {
+        const firstOctet: u8 = @intCast(i);
+        var buffer: [9]u8 = undefined;
+        const input = try std.fmt.bufPrint(&buffer, "%{X:0>2}111%A0", .{firstOctet});
+
+        if (decodeURIAlloc(testing.allocator, input)) |decoded| {
+            testing.allocator.free(decoded);
+            return error.TestUnexpectedResult;
+        } else |err| {
+            try testing.expectEqual(error.URIError, err);
+        }
+    }
+}
