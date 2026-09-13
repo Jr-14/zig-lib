93d2c348901e803cd0097359ee9f2bd6b250ef91
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-09-13 10:54:53 +1000

    test(decodeURI): complete S15.1.3.1_A1.5_T1.js test

--- src/web/decodeURI.zig
+++ src/web/decodeURI.zig
@@ -853,13 +853,35 @@ test "decodeURIAlloc: invalid leading bits (n = 5)" {
 // https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.4_T1.js
 //
 // A continuation byte (`10xxxxxx`) cannot start a UTF-8 sequence, and `11111xxx` is not a valid
-// UTF-8 starting pattern
-test "decodeURIAlloc: missing or incomplete continuation escape" {
+// UTF-8 starting pattern for B = 110xxxxx (n = 2) and (k + 2) + 3 >= length
+test "decodeURIAlloc: missing or incomplete continuation escape (n = 2)" {
     for (0xC0..0xE0) |i| {
         const firstOctet: u8 = @intCast(i);
         const suffix = "111";
         for (0..suffix.len) |suffixLength| {
             var buffer: [6]u8 = undefined;
+            const input = try std.fmt.bufPrint(&buffer, "%{X:0>2}{s}", .{ firstOctet, suffix[0..suffixLength] });
+
+            if (decodeURIAlloc(testing.allocator, input)) |decoded| {
+                testing.allocator.free(decoded);
+                return error.TestUnexpectedResult;
+            } else |err| {
+                try testing.expectEqual(error.URIError, err);
+            }
+        }
+    }
+}
+
+// https://github.com/tc39/test262/blob/main/test/built-ins/decodeURI/S15.1.3.1_A1.5_T1.js
+//
+// A continuation byte (`10xxxxxx`) cannot start a UTF-8 sequence, and `11111xxx` is not a valid
+// UTF-8 starting pattern for B = 1110xxxx (n = 3) and (k + 2) + 6 >= length
+test "decodeURIAlloc: missing or incomplete continuation escape (n = 3)" {
+    for (0xE0..0xF0) |i| {
+        const firstOctet: u8 = @intCast(i);
+        const suffix = "111111";
+        for (0..suffix.len) |suffixLength| {
+            var buffer: [9]u8 = undefined;
             const input = try std.fmt.bufPrint(&buffer, "%{X:0>2}{s}", .{ firstOctet, suffix[0..suffixLength] });
 
             if (decodeURIAlloc(testing.allocator, input)) |decoded| {
